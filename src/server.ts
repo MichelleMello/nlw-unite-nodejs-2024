86c89045ed917b91e3353af554dbd590cd358b91
Michelle Mello <mello.de.michelle@gmail.com>
2024-04-05 14:23:18 -0300

    Updated app

--- src/server.ts
+++ src/server.ts
@@ -1,50 +1,16 @@
 import fastify from "fastify";
+import { serializerCompiler, validatorCompiler, jsonSchemaTransform, ZodTypeProvider } from 'fastify-type-provider-zod'
 import {z} from "zod";
-import {PrismaClient} from '@prisma/client';
+
+import { generateSlug } from "./utils/generate-slug";
 
 const app =  fastify();
 
-const prisma = new PrismaClient({
-    log: ["query"],
-});
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
 
 
-//Métodos HTTP: GET, POST, PUT, DELETE,...
 
-//Corpo da requisição (Request Body): Dados para criação ou atualização de um registro
-//Parâmetros de busca (Search Params/Query Params): dados para filtros e paginação
-//Parâmetros de rota (Route Params): Identificar um recurso para atualização ou deleção (ex: /users/:id)  
-//Cabeçalho da requisição (Request Header): Dados sobre a requisição
-//Cabeçalho (Header): Contexto
-
-//Semânticas = Significado
-
-//Driver nativo/Query builder/ORM
-
-//Object Relational Mapping (ORM) (Hibernate / Doctrine / ActiveRecord)
-
-
-//Criar um evento
-app.post("/events", async (request, reply) => {
-    const createEventSchema = z.object({
-        title: z.string().min(4),
-        details: z.string().nullable(),
-        maximumAttendees: z.number().int().positive().nullable(),
-    })
-
-    const data = createEventSchema.parse(request.body)
-
-   const event = await prisma.event.create({
-    data: {
-        title: data.title,
-        details: data.details,
-        maximumAttendees: data.maximumAttendees,
-        slug: data.title.toLowerCase().replace(" ", "-"),
-        },
-    })
-
-        return {eventId: event.id};
-})
 
 app.listen({port: 3333}).then(() => {
     console.log("Server is running on port 3333")
